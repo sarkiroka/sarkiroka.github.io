4fc956b02efc23afab661cdcde35fb9ad8b15dea
sarkiroka <sarki_roka@freemail.hu>
2024-05-13 10:53:20 +0200

    kukucska detector

---
+++ kukucska-detector/readme.md
@@ -0,0 +1,3 @@
+# AI driven "kukucska" detector
+
+Megnyitás után (index.html), engedélyezni kell a kamerát. Amennyiben többen nézik a bejelentkező formot, akkor elrejti azt, hogy ne derüljön ki, ne nézhesse meg senki a bejelentkezési adatokat. Ha nincs senki a gép előtt, akkor is elrejti, hogy automatizált script se tudja kitölteni helyettünk.
---
+++ kukucska-detector/script.js
@@ -0,0 +1,59 @@
+const video = document.getElementById('video')
+const loginbox = document.getElementById('loginbox');
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+    const displaySize = {width: video.width, height: video.height}
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        handleDetections(detections.length);
+    }, 100)
+});
+
+let isFormVisible = false;
+let changeTimerId = 0;
+
+function handleDetections(detections) {
+    let show = detections === 1;
+    if (isFormVisible === show) {
+        clearTimeout(changeTimerId);
+        changeTimerId = 0;
+    } else {
+        if (!changeTimerId) {
+            changeTimerId = setTimeout(() => {
+                showHideForm(show);
+            }, detections === 0 ? 750 : 250);
+        }
+    }
+}
+
+let lastActiveElement = null;
+
+function showHideForm(show) {
+    isFormVisible = show;
+    if (show) {
+        loginbox.classList.add('ok');
+        loginbox.classList.remove('danger');
+        if (lastActiveElement) {
+            setTimeout(() => lastActiveElement.focus(), 1);
+        }
+    } else {
+        lastActiveElement = document.activeElement;
+        loginbox.classList.add('danger');
+        loginbox.classList.remove('ok');
+    }
+}
